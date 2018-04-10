2036b7c4417634743cfaaf2e656b3e0c5688302c
Matheus Portolan <hort_e@hotmail.com>
2018-04-10 00:09:52 -0300

    passos pedido

--- src/pages/novo-pedido/novo-pedido.ts
+++ src/pages/novo-pedido/novo-pedido.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events, AlertController, ToastController } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { NovoClientePage } from '../novo-cliente/novo-cliente';
@@ -33,6 +33,10 @@ export class NovoPedidoPage {
   clientSearch: boolean = false;
   clientFound: boolean = false;
 
+  vlrTotal: number = 0;
+  qtdTotal: number = 0;
+
+
   public pedidoForm: any;
   public condicoes: Observable<CondicaoPagamento[]>;
 
@@ -43,7 +47,8 @@ export class NovoPedidoPage {
     private condicaoPagamentoService: CondicaoPagamentoProvider,
     public pedidoService: PedidosProvider,
     private clientesProvider: ClientesProvider,
-    private alertCtrl: AlertController) {
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController) {
 
     this.condicoes = condicaoPagamentoService.condicao;
 
@@ -53,6 +58,7 @@ export class NovoPedidoPage {
       this.pedido.itens.push(novoItem);
       this.pedido.total = this.pedido.total + (novoItem.preco * novoItem.quantidade);
       console.log("total " + this.pedido.total);
+      this.calcQtde();
     });
 
     this.pedidoForm = formBuilder.group({
@@ -135,6 +141,18 @@ export class NovoPedidoPage {
     al.present();
   }
 
+
+  toastAlert(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      showCloseButton: true,
+      closeButtonText: 'Fechar',
+      dismissOnPageChange: true,
+      cssClass: 'toast-error'
+    });
+    toast.present();    
+  }
+
   onBlurCpfCnpj(value) {
 
     if (!value) {
@@ -149,6 +167,7 @@ export class NovoPedidoPage {
           this.clientFound = true;
         } else {
           this.clientFound = false;
+          this.toastAlert(this.mensagemClienteNaoEncontrado);
         }
 
         this.clientSearch = true;
@@ -175,8 +194,9 @@ export class NovoPedidoPage {
   }
 
   selecionaDadoCobrancaEContinua() {
-    if (!this.pedido.formaCobranca || !this.pedido.previsaoEntrega || !this.pedido.condicaoPagamentoId) {
+    if ( !this.pedido.previsaoEntrega ) {      
       this.mensagemDadosCobrancaNaoPreenchidos = "Todos os campos são de preenchimento obrigatório";
+      this.toastAlert(this.mensagemDadosCobrancaNaoPreenchidos);
       return;
     }
     this.passo = "4";
@@ -185,18 +205,21 @@ export class NovoPedidoPage {
   tapAddQuantidade(e, item) {
     item.quantidade++;
     this.pedido.total = this.pedido.total + (item.preco);
+    this.calcQtde();
   }
 
   tapRemoveQuantidade(e, item) {
     if (item.quantidade > 0) {
       item.quantidade--;
       this.pedido.total = this.pedido.total - (item.preco);
-    }
+      this.calcQtde();
+    }        
   }
 
   excluirItem(item) {
     this.pedido.total = this.pedido.total - (item.preco * item.quantidade);
     this.pedido.itens.pop(item);
+    this.calcQtde();    
   }
 
   editItem(item) {
@@ -204,10 +227,30 @@ export class NovoPedidoPage {
   }
 
   resumoPedido() {
+    if (this.qtdTotal <= 0){
+      this.toastAlert("Informe pelo menos um produto!");
+      return;
+    }
+
     this.passo = "5";
   }
 
   voltarAdicionarItens() {
     this.passo = "4";
   }
+
+
+  calcQtde(){
+    this.qtdTotal = 0;
+    this.pedido.itens.forEach(item => {
+      this.qtdTotal = this.qtdTotal + item.quantidade;
+    });
+  }
+
+  exibirEndereco(){
+    this.passo = "10";
+    console.log(this.pedido);
+  }
+
+
 }
